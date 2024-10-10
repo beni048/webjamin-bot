0bcea03c391d2907c6db9b875e2ee7d6861d8d00
beni048 <beni048@hotmail.com>
2024-10-10 12:56:36 +0200

    Camp tut-69

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,22 @@
 import Image from 'next/image'
-import React from 'react'
 import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col 
-    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
         <div className="hero-map" />
 
-        <div className="relative z-20 flex flex-1 flex-col xls:w-1/2">
+        <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
             <Image
                 src="/camp.svg"
                 alt="camp"
                 width={50}
                 height={50}
-                className="absolut left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+                className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
             />
             <h1 className="bold-52 lg:bold-88">Webjamin Camp Area</h1>
             <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-                We want to be on each of your journeys seeking the 
-                satisfaction of seeing the incorruptible beauty of nature. We 
-                can help you on an adventure around the world in just one app
+                We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
             </p>
 
             <div className="my-11 flex flex-wrap gap-5">
@@ -38,11 +34,10 @@ const Hero = () => {
 
                 <p className="bold-16 lg:bold-20 text-blue-70">
                     198k
-                    <span className="regular-16 lg:regular-29 ml-1">
-                        Excellent Reviews
-                    </span>
+                    <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
                 </p>
             </div>
+
             <div className="flex flex-col w-full gap-3 sm:flex-row">
                 <Button 
                     type="button" 
@@ -71,11 +66,11 @@ const Hero = () => {
 
                     <div className="flexBetween">
                         <div className="flex flex-col">
-                            <p className="regular-16 text-gray-20">Distance</p>
+                            <p className="regular-16 block text-gray-20">Distance</p>
                             <p className="bold-20 text-white">173.28 mi</p>
                         </div>
                         <div className="flex flex-col">
-                            <p className="regular-16 text-gray-20">Elevation</p>
+                            <p className="regular-16 block text-gray-20">Elevation</p>
                             <p className="bold-20 text-white">2.040 km</p>
                         </div>
                     </div>
